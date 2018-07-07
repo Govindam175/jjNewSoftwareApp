5408ef1c68666756087bedd0dfc0552084de3190
Developer1 <GOVIND@DESKTOP-DUA8OBM>
2018-07-07 23:12:31 +0530

    Create Service inteface TL

---
+++ ArithmeticApp/src/com/jj/service/IArithmetic.java
@@ -0,0 +1,10 @@
+package com.jj.service;
+
+public interface IArithmetic {
+
+	public int addition(int a,int b);
+	public int subtraction(int a,int b);
+	public int multiplication(int a,int b);
+	public float division(int a,int b);
+	
+}
